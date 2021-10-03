c27cf3a99159aa92673e6c75814cc15fe7597fa0
Tymon2115 <tymon.podlowski@gmail.com>
2021-10-03 16:25:39 +0200

    Initial commit

---
+++ FamilyData/Data/AdultService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using FileData;
+using Models;
+
+
+namespace FamilyData.Data {
+    public class AdultService : IAdultService {
+        public IList<Adult> Adults { get; private set; }
+
+        public AdultService() {
+            FileContext fileContext = new FileContext();
+            Adults = new List<Adult>(fileContext.Adults);
+        }
+
+
+        public void AddAdult(Adult adult) {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveAdult(int id) {
+            throw new System.NotImplementedException();
+        }
+
+        public void EditAdult(Adult adult) {
+            throw new System.NotImplementedException();
+        }
+
+        public Adult Get(int id) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ FamilyData/Data/IAdultService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Models;
+
+namespace FamilyData.Data {
+    public interface IAdultService {
+        IList<Adult> Adults { get;}
+        void AddAdult(Adult adult);
+        void RemoveAdult(int id);
+        void EditAdult(Adult adult);
+        Adult Get(int id);
+    }
+}
